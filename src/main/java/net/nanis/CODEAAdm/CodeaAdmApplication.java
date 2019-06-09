dd29902e98bbce2c9b5aa298c94ba9ef766b83aa
Diana <diana1310fg@gmail.com>
2019-06-08 21:18:23 -0500

    Probando...

--- src/main/java/net/nanis/CODEAAdm/CodeaAdmApplication.java
+++ src/main/java/net/nanis/CODEAAdm/CodeaAdmApplication.java
@@ -5,9 +5,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CodeaAdmApplication {
+    
+    
 
 	public static void main(String[] args) {
+            
+            
 		SpringApplication.run(CodeaAdmApplication.class, args);
+                
+                System.out.println("HOLAAAAA");
 	}
 
 }
