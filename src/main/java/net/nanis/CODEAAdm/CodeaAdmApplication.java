bb6a9111088f095bae74bd4a4aab6e4f034b9979
Diana <diana1310fg@gmail.com>
2019-06-08 21:10:26 -0500

    Inicio de App: 08/06/19. Administración de Aplicación CODEA-Usr. Starting...

---
+++ src/main/java/net/nanis/CODEAAdm/CodeaAdmApplication.java
@@ -0,0 +1,13 @@
+package net.nanis.CODEAAdm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeaAdmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeaAdmApplication.class, args);
+	}
+
+}
